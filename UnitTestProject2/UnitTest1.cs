afa88a8a2ca278b1a1d328ff7edd55ce5e5f4768
Joshuabrotman <joshuabrotman@Gmail.com>
2020-03-31 17:16:32 -0500

    1.5 more tests completed

--- UnitTestProject2/UnitTest1.cs
+++ UnitTestProject2/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
 
 namespace UnitTestProject2
 {
@@ -10,9 +11,85 @@ namespace UnitTestProject2
         public void Add_CheckCount_AddPositiveInteger()
         {
 
-            //Arrange
-            //Act
-            //Assert
+            //arrange
+            ArrayList<int> testList1 = new ArrayList<int>();
+            int value1 = 5;
+            int expected = 1;
+            int actual;
+            //act
+            testList1.Add(value1);
+            actual = testList1.Count;
+            //assert
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Add_CheckIndex_ReturnsValueOfIndex()
+        {
+
+            //arrange
+            ArrayList<int> testList1 = new ArrayList<int>();
+            int value1 = 5;
+            int expected = 5;
+            int actual;
+            //act
+            testList1.Add(value1);
+            actual = testList1[0];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        //add an element, make sure it exists
+        [TestMethod]
+        public void Add_CheckElement_ReturnsElementsJustAdded()
+        {
+
+            //arrange
+            int actual;
+            ArrayList<int> testList1 = new ArrayList<int>();
+
+            //act
+            testList1.Add(5);
+            actual = testList1[0];
+
+
+            //assert
+            Assert.AreEqual(5, actual);
+
+        }
+
+        //add one int element, make sure original is still present
+        [TestMethod]
+        public void Add_CheckElement_IsOriginalStillThere()
+        {
+            //arrange
+            int actual;
+            ArrayList<int> testList1 = new ArrayList<int>();
+
+            //act
+            testList1.Add(5);
+            actual = testList1[0];
+
+
+            //assert
+            Assert.AreEqual(5, actual);
+        }
+        //test that capacity increases accordingly
+
+        [TestMethod]
+        public void Add_CheckCapacity_DoesCapacityExpland()
+        {
+            //arrange
+            ArrayList<int> testList1 = new ArrayList<int>();
+
+            //act
+            testList1.Add(32);
+            testList1.Add(43);
+            testList1.Add(89);
+            testList1.Add(11);
+            //assert
+        }
+
     }
 }
