2110db4c7844e72ff06fe6cc30f996c03d775803
Joshuabrotman <joshuabrotman@Gmail.com>
2020-04-01 15:42:46 -0500

    Added five remove tests

--- UnitTestProject2/UnitTest1.cs
+++ UnitTestProject2/UnitTest1.cs
@@ -7,6 +7,100 @@ namespace UnitTestProject2
     [TestClass]
     public class UnitTest1
     {
+
+        [TestMethod]
+        public void Remove_CheckCountIsLower_RemovePositiveInteger()
+        {
+            //arrange
+            
+            int expected = 2;
+            int actual;
+            //act
+            ArrayList<int> testlist = new ArrayList<int>();
+            testlist.Add(99);
+            testlist.Add(43);
+            testlist.Add(55);
+            testlist.Remove(55);
+            actual = testlist.Count;
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Remove_CheckIndex1IsChanged_RemoveIndex1()
+        {
+            //arrange
+            int expected = 12;
+            int actual;
+            //act
+            ArrayList<int> testlist = new ArrayList<int>();
+            testlist.Add(99);
+            testlist.Add(43);
+            testlist.Add(12);
+            testlist.Remove(43);
+            actual = testlist[1];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Remove_Check1stIndexRemains_RemoveIndex1()
+        {
+            //arrange
+            int expected = 99;
+            int actual;
+            //act
+            ArrayList<int> testlist = new ArrayList<int>();
+            testlist.Add(99);
+            testlist.Add(43);
+            testlist.Add(12);
+            testlist.Remove(43);
+            actual = testlist[0];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Remove_CheckTopElementsShiftDown_RemoveIndex1()
+        {
+            //arrange
+            int expected = 22;
+            int actual;
+            //act
+            ArrayList<int> testlist = new ArrayList<int>();
+            testlist.Add(99);
+            testlist.Add(43);
+            testlist.Add(12);
+            testlist.Add(22);
+            testlist.Remove(43);
+            actual = testlist[2];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        public void Remove_CheckCountIsStillTheSame_RemoveNonExistentItem()
+        {
+            //arrange
+            int expected = 3;
+            int actual;
+            //act
+            ArrayList<int> testlist = new ArrayList<int>();
+            testlist.Add(99);
+            testlist.Add(43);
+            testlist.Add(12);
+            testlist.Add(22);
+            testlist.Remove(66);
+            actual = testlist.Count;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
         [TestMethod]
         public void Add_CheckCount_AddPositiveInteger()
         {
